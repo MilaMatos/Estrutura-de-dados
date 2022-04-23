b7352002238eb0fa8ca3441f0a4a87b39f541a85
Camila Vanessa <49792175+MilaMatos@users.noreply.github.com>
2022-04-23 13:40:18 -0300

    Update prova1.h

--- prova1.h
+++ prova1.h
@@ -8,8 +8,9 @@
 typedef struct elemento* Lista;
 
 Lista* criar();
-int inserirInicio(Lista*, struct professor);  //Inserir no inicio sem repetição de Id
-int imprimir(Lista*);
+int inserirInicio(Lista*, struct professor);   //Inserir no inicio sem repetiÃ§Ã£o de Id
+int imprimir(Lista*);						  //Imprime os elementos da lista	
 void info(Lista*); 						     //Dizer quantos impares, pares, maior e menor valor de uma lista
 int remover2Inicio(Lista*);					//remover os 2 primeiros
 int remover2Fim(Lista*);				   //remover os 2 ultimos
+
